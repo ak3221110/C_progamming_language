b87c0dd5b52ee39334be3659d59a81aeea3b55b0
Abhishek Kumar <abhishek401796@outlook.com>
2024-07-29 00:24:13 +0530

    substraction code is added now

--- projects/calculater.c
+++ projects/calculater.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-void sum(){
+ void sum(){
     int a,b,c,d;
-    int optation;
+    int Option;
     printf("Enter First number:");
     scanf("%d",&a);
     printf("Enter Secound number:");
@@ -12,26 +12,58 @@ void sum(){
      
     printf("Your Result of addition %d and %d is:   %d\n",a,b,c);
     printf("\nAre you want Add another number please enter(0/1) \nhints 0 for no  1 for yes\n enter :");
-    scanf("%d",&optation);
-     if (optation == 1)
+    scanf("%d",&Option);
+     if (Option == 1)
      {
        printf("Enter a next number:");
        scanf("%d",&d);
        c=c+d;
        printf("Your Result of addition %d , %d and %d is: %d\n",a,b,d,c);
      }
-     else if(optation == 0){
+     else if(Option == 0){
         printf("OK, Your result is:%d",c);
         return 0;
      }
      else{
         printf("sorry Your result is:%d",c);
         return 0;
-     }}
+     }}}
+     
+     
+     
+void sub(){
+    int a,b,c,d,Option;
     
-
-}
+    printf("Enter First number:");
+    scanf("%d",&a);
+    printf("Enter Secound number:");
+    scanf("%d",&b);
+    c=a-b;
+    while (1)
+    {
+     
+    printf("Your Result of Subtraction %d and %d is:   %d\n",a,b,c);
+    printf("\nAre you want substract another number please enter(0/1) \nhints 0 for no  1 for yes\n enter :");
+    scanf("%d",&Option);
+     if (Option == 1)
+     {
+       printf("Enter a next number:");
+       scanf("%d",&d);
+       c=c-d;
+       printf("Your Result of Subtraction %d , %d and %d is: %d\n",a,b,d,c);
+     }
+     else if(Option == 0){
+        printf("OK, Your result is:%d",c);
+        return 0;
+     }
+     else{
+        printf("sorry Your result is:%d",c);
+        return 0;
+     }}}
+    
+    
 int main(){
     sum();
+    sub();
     return 0;
 }
