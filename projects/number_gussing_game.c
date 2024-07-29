a8d5f2bbfb32c0dc38e8b5adcf2d156211ee0df5
Abhishek Kumar <abhishek401796@outlook.com>
2024-07-29 17:30:32 +0530

    number gussing game

---
+++ projects/number_gussing_game.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int e_number, g_number, attempt;
+    g_number=rand()/100+1;
+    printf("%d",g_number);
+    
+    printf("\nWelcome to 'number gussing game'");
+    printf("\nPlease enter any number between 1-100:   ");
+    scanf("%d  \n",&e_number);
+    attempt=0;
+
+    while (1){
+        attempt++;
+        if (g_number < e_number)
+        {
+           printf("Please Guss a smmaller number:");
+           scanf("%d",&e_number);
+        }
+        else if (g_number > e_number)
+        {
+           printf("Please Guss a larger number:");
+           scanf("%d",&e_number);
+        }
+        else {
+           printf("Your are sussfully gussed number %d in %d attemped",g_number,attempt);
+           return 0;
+          
+        }
+        
+    }
+    
+    printf("Developed By: ABHISHEK KUMAR");
+
+    return 0;
+}
